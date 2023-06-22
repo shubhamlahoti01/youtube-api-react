eb8b067491f0cb05c48c9f075f1bb25919720082
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-06-22 19:05:53 +0530

    first

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "cb4120d286mshfada207ad1fc3d0p19b107jsnfd1f8ddee974",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  // console.log(data);
+  return data;
+};
